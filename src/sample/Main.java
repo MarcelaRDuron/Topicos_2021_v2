3d603ecfcbf788cde62f8e6c82ddaf4cc4492954
MarcelaRDuron <18031509@itcelaya.edu.mx>
2021-08-30 18:29:59 -0500

    Creacion del repositorio

---
+++ src/sample/Main.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    Button btn1, btn2, btn3;
+    HBox hBox;
+    VBox vBox;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        btn1 = new Button("Botón1");
+        btn2 = new Button("Botón2");
+        btn3 = new Button("Botón3");
+        hBox = new HBox();
+        hBox.setSpacing(5.0);
+        hBox.getChildren().addAll(btn1, btn2, btn3);
+
+        vBox= new VBox();
+        vBox.setSpacing(5.0);
+        vBox.getChildren().addAll(btn1,btn2,btn3);
+
+        primaryStage.setTitle("Tópicos Avanzados de Programación");
+        primaryStage.setScene(new Scene(vBox, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
